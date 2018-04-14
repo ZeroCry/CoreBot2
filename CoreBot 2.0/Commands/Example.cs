73dfbe09bb9643840d4dd3703678de31cf053355
PassiveModding <passivemodding@gmail.com>
2018-04-14 23:30:11 +1000

    Added simple help command

--- CoreBot 2.0/Commands/Example.cs
+++ CoreBot 2.0/Commands/Example.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 
@@ -9,10 +11,68 @@ namespace CoreBot.Commands
 {
     public class Example : ModuleBase
     {
+        public readonly CommandService _service;
+
+        public Example(CommandService service)
+        {
+            _service = service;
+        }
+
         [Command("Example")]
         public async Task ExampleCommand()
         {
             await ReplyAsync("This is an example command");
         }
+
+
+        [Command("help")]
+        [Summary("help")]
+        [Remarks("all help commands")]
+        public async Task HelpAsync([Remainder] string modulearg = null)
+        {
+            var embed = new EmbedBuilder
+            {
+                Color = new Color(114, 137, 218),
+                Title = $"{Context.Client.CurrentUser.Username} Commands"
+            };
+            var prefix = Config.Load().Prefix;
+            if (modulearg == null) //ShortHelp
+            {
+                var modules = _service.Modules.Where(x => x.Commands.Count > 0).Select(x => new simplemoduleobj
+                {
+                    moduledesc = string.Join("\n",
+                        x.Commands.Select(c => $"`{prefix}{c.Summary ?? c.Name}` {(c.Remarks == null ? "" : $"- {c.Remarks}")}")),
+                    modulename = x.Name
+                });
+                foreach (var module in modules)
+                {
+                    embed.AddField(module.modulename, module.moduledesc);
+                }
+
+                await ReplyAsync("", false, embed.Build());
+            }
+            else
+            {
+                var modules = _service.Modules.Where(x => x.Commands.Count > 0).Where(x => string.Equals(x.Name, modulearg, StringComparison.CurrentCultureIgnoreCase)).Select(x => new simplemoduleobj
+                {
+                    moduledesc = string.Join("\n",
+                        x.Commands.Select(c => $"`{prefix}{c.Summary ?? c.Name}` {(c.Remarks == null ? "" : $"- {c.Remarks}")}")),
+                    modulename = x.Name
+                });
+                foreach (var module in modules)
+                {
+                    embed.AddField(module.modulename, module.moduledesc);
+                }
+
+                await ReplyAsync("", false, embed.Build());
+            }
+        }
+
+        public class simplemoduleobj
+        {
+            public string modulename { get; set; }
+            public string moduledesc { get; set; }
+        }
+
     }
 }
